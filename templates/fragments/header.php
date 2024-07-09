90d6d4eb5dfa064b093a4d73c3bb48a74a264641
Titou1206 <laugierantoine@gmail.com>
2024-07-09 16:20:38 +0200

    Initial commit

---
+++ templates/fragments/header.php
@@ -0,0 +1,6 @@
+<div class="containFond">
+        <div class="fond <?= $fonds[$pos] ?>"></div>
+    </div>
+<div class="titre flex a-center">
+    <h1 class="txt-center yellow w100 fs20">💥 Epic Clash : Lost Dungeon 💥</h1>
+</div>
